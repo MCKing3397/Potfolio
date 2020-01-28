d373c2a0aa6a1c88874d759b71e470f56d9c0bf9
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-01-28 10:29:41 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <HelloWorld msg="This is a test"/>
   </div>
 </template>
 
