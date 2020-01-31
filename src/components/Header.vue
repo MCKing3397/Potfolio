74d8962780d2d64fa9a1217fa6a792b3969c0b30
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-01-31 10:54:23 -0600

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,18 @@
 <template>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="icon" href="<%= BASE_URL %>favicon.ico">
-    <title>{{title}}</title>
-  </head>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <link rel="icon" href="<%= BASE_URL %>favicon.ico">
+  <title>{{ title }}</title>
+</head>
 </template>
 
 <script>
 export default {
   name: 'Header',
   props: {
-  	title: String
+    title: String
   }
 }
 </script>
