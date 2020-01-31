f73ca05fdabde0fe8caf5e0adb2f494d0d5a8be6
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-01-31 11:06:12 -0600

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,6 +5,10 @@
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <link rel="icon" href="<%= BASE_URL %>favicon.ico">
   <title>{{ title }}</title>
+  <link href="/css/app.1b3c6512.css" rel="preload" as="style">
+  <link href="/js/app.bac73e92.js" rel="preload" as="script">
+  <link href="/js/chunk-vendors.e4831489.js" rel="preload" as="script">
+  <link href="/css/app.1b3c6512.css" rel="stylesheet"
 </head>
 </template>
 
