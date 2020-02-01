c1bac8997b6b6557aab9a1d88afcc281d19b51a6
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-01-31 19:12:18 -0600

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,9 +5,6 @@
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <link rel="icon" href="<%= BASE_URL %>favicon.ico">
   <title>{{ title }}</title>
-  <link href="/css/app.1b3c6512.css" rel="preload" as="style">
-  <link href="/js/app.bac73e92.js" rel="preload" as="script">
-  <link href="/js/chunk-vendors.e4831489.js" rel="preload" as="script">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
 </head>
 </template>
