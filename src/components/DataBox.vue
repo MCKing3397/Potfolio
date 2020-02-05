7dd35edf14930f4b92066ba5332cb6f89712f205
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-02-05 14:58:18 -0600

    Update DataBox.vue

--- src/components/DataBox.vue
+++ src/components/DataBox.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="databox tile is-ancestor">
+  <div class="databox tile columns is-centered is-ancestor">
     <div class="tile is-parent"> 
 
-      <article class="tile is-child is-4 is-offset-4 box">
+      <article class="tile is-child is-4 box is-radiusless">
         <p class="title">{{ title }}</p>
       </article>
 
-      <article class="tile is-4 is-child box">
+      <article class="tile is-4 is-child box is-radiusless is-vcentered">
         <p class="content">{{ msg }}</p>
       </article>
     </div>
