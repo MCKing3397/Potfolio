0801d69025744009dbc65f2fb81f8d9e94fd3820
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-02-05 14:07:29 -0600

    Update DataBox.vue

--- src/components/DataBox.vue
+++ src/components/DataBox.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="databox columns is-gapless">
-    <div class="column is-4 is-offset-4">
-      <article class="box">
+  <div class="databox columns is-gapless is-vcentered tile is-ancestor">
+    <div class="column is-4 is-offset-4 tile is-parent">
+      <article class="tile box is-child">
         <p class="title">{{ title }}</p>
       </article>
     </div>
-    <div class="column is-4">
-      <article class="box">
+    <div class="column is-4 tile is-parent">
+      <article class="tile box is-child">
         <div class="content">
           <p>{{ msg }}</p>
         </div>
