eca2ea7a84cc04fb53e46cc8995b557b88de8745
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-02-04 21:36:54 -0600

    Update DataBox.vue

--- src/components/DataBox.vue
+++ src/components/DataBox.vue
@@ -17,27 +17,10 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: DataBox',
   props: {
-    msg: String
+    msg: String,
+    title: String
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
