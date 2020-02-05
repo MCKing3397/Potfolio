cb9cf5ccfe121ae236c1e2b1debb90bc27239162
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-02-04 21:34:56 -0600

    Update DataBox.vue

--- src/components/DataBox.vue
+++ src/components/DataBox.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="databox is-ancestor tile">
     <div class="tile is-parent is-gapless">
-    <article class="tile is-child box">
-      <p class="title">{{ title }}</p>
-    </article>
-  </div>
-  <div class="tile is-parent is-gapless">
-    <article class="tile is-child box">
-      <div class="content">
-        <p>{{ msg }}</p>
-      </div>
-    </article>
-  </div>
-  </div>
-</div>
+      <article class="tile is-child box">
+        <p class="title">{{ title }}</p>
+      </article>
+    </div>
+    <div class="tile is-parent is-gapless">
+      <article class="tile is-child box">
+        <div class="content">
+          <p>{{ msg }}</p>
+        </div>
+      </article>
+    </div>
   </div>
 </template>
 
