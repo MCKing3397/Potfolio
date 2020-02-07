84d8923483b3e8936ebd429a5c31b98f3dec2fc9
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-02-07 15:58:27 -0600

    Add v-bind

--- src/components/DataBox.vue
+++ src/components/DataBox.vue
@@ -7,7 +7,7 @@
 
       <article class="tile is-child is-4 box is-radiusless">
         <p class="content" v-if="msg">{{ msg }}</p>
-        <p class="content" v-else><ul><li v-for="item in skills">{{ item }}</li></ul></p>
+        <p class="content" v-else><ul><li v-for="item in skills" v-bind:item="item" v-bind:key="item">{{ item }}</li></ul></p>
       </article>
     </div>
   </div>
