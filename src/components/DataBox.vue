21500866cf20ac5aad7378b5de7980464938be64
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-02-07 15:25:09 -0600

    Update DataBox.vue

--- src/components/DataBox.vue
+++ src/components/DataBox.vue
@@ -6,8 +6,8 @@
       </article>
 
       <article class="tile is-child is-4 box is-radiusless">
-        <p class="content" v-if="skills != ''"><ul><li v-for="item in skills">{{ item }}</li></ul></p>
-        <p class="content" v-else>{{ msg }}</p>
+        <p class="content" v-if="msg">{{ msg }}</p>
+        <p class="content" v-else><ul><li v-for="item in skills">{{ item }}</li></ul></p>
       </article>
     </div>
   </div>
