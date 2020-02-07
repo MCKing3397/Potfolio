88b5906ed383ad26756e58069976e8d1f4992c58
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-02-07 16:11:45 -0600

    Update DataBox.vue

--- src/components/DataBox.vue
+++ src/components/DataBox.vue
@@ -7,7 +7,7 @@
 
       <article class="tile is-child is-4 box is-radiusless">
         <p class="content" v-if="msg">{{ msg }}</p>
-        <p class="content" v-else><ul><li v-for="item in skills" v-bind:item="item." v-bind:key="item.id">{{ item.skill }} <br> {{ item.level }}</li></ul></p>
+        <p class="content" v-else><ul><li v-for="item in skills" v-bind:item="item.item" v-bind:key="item.id">{{ item.skill }} <br> {{ item.level }}</li></ul></p>
       </article>
     </div>
   </div>
