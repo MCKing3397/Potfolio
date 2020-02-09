8cba223b642e9c0ba8d6e4c21116dd856953dcf3
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-02-08 21:46:22 -0600

    Add skill bar

--- src/components/DataBox.vue
+++ src/components/DataBox.vue
@@ -7,7 +7,7 @@
 
       <article class="tile is-child is-4 box is-radiusless">
         <p class="content" v-if="msg">{{ msg }}</p>
-        <p class="content" v-else><ul><li v-for="item in skills" v-bind:item="item.item" v-bind:key="item.id">{{ item.skill }} <br> {{ item.level }}</li></ul></p>
+        <p class="content" v-else><ul><li v-for="item in skills" v-bind:key="item.id"><progress class="progress is-white" v-bind:value="item.level * 10" max="100"></progress> {{ item.level }}</li></ul></p>
       </article>
     </div>
   </div>
---
+++ src/components/skillBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <progress class="progress is-danger" v-bind:value="level" max="100">90%</progress>
+</template>
+<script>
+
+export default {
+  name: 'skillBar',
+  props: {
+    skill: String,
+    level: Number
+  }
+}
+</script>
