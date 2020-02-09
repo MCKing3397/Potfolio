e9431422679949600bdf2bcb0500f6764cf22452
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-02-08 22:01:15 -0600

    Add skill names

--- src/components/DataBox.vue
+++ src/components/DataBox.vue
@@ -7,7 +7,7 @@
 
       <article class="tile is-child is-4 box is-radiusless">
         <p class="content" v-if="msg">{{ msg }}</p>
-        <p class="content" v-else><ul><li v-for="item in skills" v-bind:key="item.id"><progress class="progress is-white" v-bind:value="item.level * 10" max="100">{{ item.level }}</progress></li></ul></p>
+        <p class="content" v-else><ul><li v-for="item in skills" v-bind:key="item.id"><label v-bind:for="item.skill">{{ item.skill }}:</label><progress class="progress is-white" v-bind:value="item.level * 10" max="100"></progress></li></ul></p>
       </article>
     </div>
   </div>
