d045db5c7ee60d62444bdc3e5f2d3a9c6a2aa307
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-02-10 15:40:38 -0600

    Add SVG header

--- src/components/page_top.vue
+++ src/components/page_top.vue
@@ -1,48 +1,49 @@
 <template>
   <div class="page_top">
     <section class="hero is-small" id="top_hero">
-  <!-- Hero head: will stick at the top -->
+      <!-- Hero head: will stick at the top -->
 
-  <div class="hero-head">
-    <div class="columns">
-        <div class="column">
-          <h1 class="title has-text-white has-text-centered is-1">
-            Michael King
-          </h1>
-          <h2 class="title has-text-white has-text-centered is-2">
-            Web Developer
-          </h2>
+      <div class="hero-head">
+        <div class="columns">
+            <div class="column">
+              <h1 class="title has-text-white has-text-centered is-1">
+                Michael King
+              </h1>
+              <h2 class="title has-text-white has-text-centered is-2">
+                Web Developer
+              </h2>
+            </div>
+            <div class="column">
+            </div>
         </div>
-        <div class="column">
-        </div>
-    </div>
-  </div>
-
-  <!-- Hero content: will be in the middle -->
-  <div class="hero-body">
-        <div class="column">
-          <figure class="image is-128x128">
-            <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-          </figure>
-        </div>
-  </div>
-
-  <!-- Hero footer: will stick at the bottom -->
-  <div class="hero-foot">
-    <!--nav class="tabs">
-      <div class="container">
-        <ul>
-          <li class="is-active"><a>Overview</a></li>
-          <li><a>Modifiers</a></li>
-          <li><a>Grid</a></li>
-          <li><a>Elements</a></li>
-          <li><a>Components</a></li>
-          <li><a>Layout</a></li>
-        </ul>
       </div>
-    </nav-->
-  </div>
-</section>
+
+      <!-- Hero content: will be in the middle -->
+      <div class="hero-body">
+            <div class="column">
+              <figure class="image is-128x128">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+              </figure>
+            </div>
+      </div>
+
+      <!-- Hero footer: will stick at the bottom -->
+      <div class="hero-foot">
+        <!--nav class="tabs">
+          <div class="container">
+            <ul>
+              <li class="is-active"><a>Overview</a></li>
+              <li><a>Modifiers</a></li>
+              <li><a>Grid</a></li>
+              <li><a>Elements</a></li>
+              <li><a>Components</a></li>
+              <li><a>Layout</a></li>
+            </ul>
+          </div>
+        </nav-->
+      </div>
+      <div class="page_top-bottom"></div>
+    </section>
   </div>
 </template>
 
