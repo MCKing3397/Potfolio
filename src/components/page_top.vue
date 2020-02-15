3722f7ef748d487ce8bbdffb5a08f591e6bc39bc
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-02-15 15:35:01 -0600

    Began styling header

--- src/components/page_top.vue
+++ src/components/page_top.vue
@@ -20,23 +20,25 @@
 
       <!-- Hero content: will be in the middle -->
       <div class="hero-body">
-        <div class="columns">
-            <div class="column">
-              <figure class="image">
-                <img class="header_image" src="https://bulma.io/images/placeholders/128x128.png">
-              </figure>
-            </div>
-            <div class="column">
-              <div id="header_about">
-                <h2>Want to know some about me?</h2>
-                <p>My name is <span id="header_name">Michael King</span>.</p>
-                <p>I create and design responsive websites, from the UX/design phase to the front-end, with a few back-end touches.</p>
-
-                <p>I am an aspiring front-end developer and UX designer. I am currently studying Business Information Systems at Mississippi State University and can be found using my skills to create websites for Mississippi State's College of Forest Resources as a student web developer.</p>
-
-                <p>Although I have experience in back-end as well as the front-end, my goal moving forward is to follow my passion as a front-end developer.</p>
+        <div class="container">
+          <div class="columns">
+              <div class="column">
+                <figure class="image">
+                  <img class="header_image" src="https://bulma.io/images/placeholders/480x600.png">
+                </figure>
               </div>
-            </div>
+              <div class="column">
+                <div id="header_about">
+                  <h1 class="is-size-1">Want to know some about me?</h2>
+                  <p>My name is <span id="header_name" class="has-text-weight-semibold">Michael King</span>.</p>
+                  <p>I create and design responsive websites, from the UX/design phase to the front-end, with a few back-end touches.</p>
+
+                  <p>I am an aspiring front-end developer and UX designer. I am currently studying Business Information Systems at Mississippi State University and can be found using my skills to create websites for Mississippi State's College of Forest Resources as a student web developer.</p>
+
+                  <p>Although I have experience in back-end as well as the front-end, my goal moving forward is to follow my passion as a front-end developer.</p>
+                </div>
+              </div>
+          </div>
         </div>
       </div>
 
