df2eb022077d272d6fd85c4f793be3687c8abffd
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-03-14 14:54:27 -0500

    Update page_top.vue

--- src/components/page_top.vue
+++ src/components/page_top.vue
@@ -25,7 +25,7 @@
                 </a>
                 <span class="navbar-item">
                   <a class="button is-success is-inverted" target="_blank" href="https://www.github.com/mcking3397">
-                    <font-awesome-icon icon="github" />
+                    <font-awesome-icon icon="GitHub" />
                     <span>My Github</span>
                   </a>
                 </span>
