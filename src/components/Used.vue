76213fbdbb0b3b492723f63c08154eeb4768c3a2
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-03-16 11:38:03 -0500

    Worked on tiles

--- src/components/Used.vue
+++ src/components/Used.vue
@@ -2,9 +2,9 @@
 
   <div class="section">
     <div class="tile is-ancestor" id="usedDiv">
-      <div class="tile is-parent columns is-centered" v-for="tech in usedTech" v-bind:key="tech.id">
-        <a href="{{tech.link}}" target="_blank"><article class="tile is-child is-4 box is-radiusless" style="background-image: {{tech.image}}">
-          <p class="content"><span class="has-text-4">{{ tech.name }}</span><span>{{tech.description}}</span></p>
+      <div class="tile is-parent is-4" v-for="tech in usedTech" v-bind:key="tech.id">
+        <a :href="{{tech.link}}" target="_blank"><article class="tile is-child" :style="background-image: tech.image">
+          <p class="content"><span class="title">{{ tech.name }}</span></p>
         </article>
       </div>
     </div>
