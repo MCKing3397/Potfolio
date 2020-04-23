602db390f8c3221c67de699723a11d3aa05bb5cd
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-22 21:48:21 -0500

    Re-added Used Tech component

--- src/components/Used.vue
+++ src/components/Used.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="section">
-    <!--div class="tile is-ancestor" id="usedDiv">
+    <div class="tile is-ancestor" id="usedDiv">
       <div class="tile is-parent is-4" v-for="tech in usedTech" v-bind:key="tech.id">
         <a href="test.link" target="_blank"><article class="tile is-child" :style="background-image: tech.image">
-          <p class="content"><span class="title">{{ tech.name }}</span></p>
+          <p class="content"><span class="title">{{ tech.title }}</span></p>
         </article>
       </div>
-    </div-->
+    </div>
   </div>
 </template>
 <script>
