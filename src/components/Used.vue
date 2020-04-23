34dce55fce5d63f182675c0700fd7206deb56d2a
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-22 22:00:19 -0500

    Update Used.vue

--- src/components/Used.vue
+++ src/components/Used.vue
@@ -2,9 +2,10 @@
   <div class="section">
     <div class="tile is-ancestor" id="usedDiv">
       <div class="tile is-parent is-4" v-for="tech in usedTech" v-bind:key="tech.id">
-        <a href="test.link" target="_blank"><article class="tile is-child" :style="background-image: tech.image">
+        <!--a href="test.link" target="_blank"><article class="tile is-child" :style="background-image: tech.image">
           <p class="content"><span class="title">{{ tech.title }}</span></p>
-        </article></a>
+        </article></a-->
+        <p>This is a test</p>
       </div>
     </div>
   </div>
