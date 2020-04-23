ee685c2b581fcd3f57290ce09af68bff38378bb0
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-22 22:48:36 -0500

    center text and align images

--- src/components/Used.vue
+++ src/components/Used.vue
@@ -3,10 +3,10 @@
     <div class="tile is-ancestor" id="usedDiv">
       <div class="tile is-parent is-4" v-for="tech in usedTech" v-bind:key="tech.id">
         <a v-bind:href="tech.link" target="_blank"><article class="tile is-child box">
-          <figure class="image is-4by3">
+          <figure class="image is-3by1">
             <img v-bind:src="tech.image">
           </figure>
-          <p class="content"><span class="title">{{ tech.title }}</span></p>
+          <p class="content has-text-centered"><span class="title">{{ tech.title }}</span></p>
         </article></a>
       </div>
     </div>
