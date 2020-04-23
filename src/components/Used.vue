b990090f9f75e98e75c2fc3aff1f308610706c95
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-22 22:03:05 -0500

    Added v-bind

--- src/components/Used.vue
+++ src/components/Used.vue
@@ -2,10 +2,9 @@
   <div class="section">
     <div class="tile is-ancestor" id="usedDiv">
       <div class="tile is-parent is-4" v-for="tech in usedTech" v-bind:key="tech.id">
-        <!--a href="test.link" target="_blank"><article class="tile is-child" :style="background-image: tech.image">
+        <a v-bind:href="tech.link" target="_blank"><article class="tile is-child" v-bind:style="background-image: tech.image">
           <p class="content"><span class="title">{{ tech.title }}</span></p>
-        </article></a-->
-        <p>This is a test</p>
+        </article></a>
       </div>
     </div>
   </div>
