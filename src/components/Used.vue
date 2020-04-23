1bbe2a7fd40b98e41fb6c13bd39ede44aeac554c
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-22 22:31:06 -0500

    Added new Bulma logo and worked on boxes

--- src/components/Used.vue
+++ src/components/Used.vue
@@ -2,7 +2,10 @@
   <div class="section">
     <div class="tile is-ancestor" id="usedDiv">
       <div class="tile is-parent is-4" v-for="tech in usedTech" v-bind:key="tech.id">
-        <a v-bind:href="tech.link" target="_blank"><article class="tile is-child" v-bind:style="{backgroundImage: 'url(' + tech.image + ')'}">
+        <a v-bind:href="tech.link" target="_blank"><article class="tile is-child box">
+          <figure class="image is-4x3">
+            <img v-bind:src="tech.image">
+          </figure>
           <p class="content"><span class="title">{{ tech.title }}</span></p>
         </article></a>
       </div>
