d003ec9fb2736b7b09caec82dd2c26f6cbdbc7a7
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-25 21:12:03 -0500

    Test that will probably fail

---
+++ src/components/codePen.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+        <p class="codepen" data-height="265" data-theme-id="default" data-default-tab="result" data-user="Michael_King" data-slug-hash="vYEbLGL" style="height: 265px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;" data-pen-title="Tabs with Transition Effect">
+      <span>See the Pen <a href="https://codepen.io/Michael_King/pen/vYEbLGL">
+      Tabs with Transition Effect</a> by Michael (<a href="https://codepen.io/Michael_King">@Michael_King</a>)
+      on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+    <script async src="https://static.codepen.io/assets/embed/ei.js"></script>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'codePen',
+  props: {
+    usedTech: Array
+  }
+}
+</script>
