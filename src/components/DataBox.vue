7b6ed8e6a244a9828bb9c1f57f2c23657e1ab8d9
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-25 20:48:19 -0500

    Testing adding section on databox

--- src/components/DataBox.vue
+++ src/components/DataBox.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="databox tile is-ancestor">
-    <div class="tile is-parent columns is-centered"> 
-      <article class="tile is-child is-4 box is-radiusless is-flex align-center">
-        <p class="title">{{ title }}</p>
-      </article>
+  <div class="content">
+    <div class="databox tile is-ancestor">
+      <div class="tile is-parent columns is-centered"> 
+        <article class="tile is-child is-4 box is-radiusless is-flex align-center">
+          <p class="title">{{ title }}</p>
+        </article>
 
-      <article class="tile is-child is-4 box is-radiusless">
-        <p class="content" v-if="msg">{{ msg }}</p>
-        <p class="content" v-else><ul><li v-for="item in skills" v-bind:key="item.id"><label v-bind:for="item.skill">{{ item.skill }}:</label><progress class="progress is-white" v-bind:id="item.skill" v-bind:value="item.level * 10" max="100"></progress></li></ul></p>
-      </article>
+        <article class="tile is-child is-4 box is-radiusless">
+          <p class="content" v-if="msg">{{ msg }}</p>
+          <p class="content" v-else><ul><li v-for="item in skills" v-bind:key="item.id"><label v-bind:for="item.skill">{{ item.skill }}:</label><progress class="progress is-white" v-bind:id="item.skill" v-bind:value="item.level * 10" max="100"></progress></li></ul></p>
+        </article>
+      </div>
     </div>
   </div>
 </template>
