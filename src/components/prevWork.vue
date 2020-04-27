6afdeee8b5ccc1f1f72703675b011c3efb8c17a3
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-26 22:57:39 -0500

    Add example carousel

--- src/components/prevWork.vue
+++ src/components/prevWork.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="container">
-    <carousel>
-      <slide>
-        Slide 1 Content
-      </slide>
-      <slide>
-        Slide 2 Content
-      </slide>
+    <carousel :scrollPerPage="true" :perPageCustom="[[480, 2], [768, 3]]"> 
+      <slide><img src="https://picsum.photos/300/100/"></slide>
+      <slide><img src="https://picsum.photos/300/100/"></slide>
+      <slide><img src="https://picsum.photos/300/100/"></slide>
+      <slide><img src="https://picsum.photos/300/100/"></slide>
+      <slide><img src="https://picsum.photos/300/100/"></slide>
     </carousel>
   </div>
 </template>
