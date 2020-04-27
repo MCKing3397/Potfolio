381908c8e1ef0b588b7c936105acfbf4d5f9910e
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-26 23:30:26 -0500

    Style Worked boxes

--- src/components/prevWork.vue
+++ src/components/prevWork.vue
@@ -3,12 +3,7 @@
     <div class="tile is-ancestor" id="usedDiv">
       <div class="tile is-parent is-4" v-for="site in sites" v-bind:key="site.id">
         <a v-bind:href="site.link" target="_blank">
-          <article class="tile is-child box">
-            <figure class="image">
-              <img v-bind:src="site.image">
-            </figure>
-            <p class="content has-text-centered"><span class="title">{{ site.title }}</span></p>
-          </article>
+          <article class="tile is-child box workBox" :style="{background: 'url('+site.image+')'}">
         </a>
       </div>
     </div>
