580aaac045f000fef1f25b7f719e5cf2f9c1c43d
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-26 23:13:52 -0500

    Make tiles wrap

--- src/components/prevWork.vue
+++ src/components/prevWork.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="container">
     <div class="tile is-ancestor" id="usedDiv">
-      <div class="tile is-parent is-12">
-        <div class="tile is-parent is-4" v-for="site in sites" v-bind:key="site.id">
-          <a v-bind:href="site.link" target="_blank">
-            <article class="tile is-child box">
-              <figure class="image">
-                <img v-bind:src="site.image">
-              </figure>
-              <p class="content has-text-centered"><span class="title">{{ site.title }}</span></p>
-            </article>
-          </a>
-        </div>
+      <div class="tile is-parent is-4" v-for="site in sites" v-bind:key="site.id">
+        <a v-bind:href="site.link" target="_blank">
+          <article class="tile is-child box">
+            <figure class="image">
+              <img v-bind:src="site.image">
+            </figure>
+            <p class="content has-text-centered"><span class="title">{{ site.title }}</span></p>
+          </article>
+        </a>
       </div>
     </div>
   </div>
