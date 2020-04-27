26767c8da2ca1bd6eed2226eef5a41e42fe2eb23
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-26 23:35:06 -0500

    More workBox styling

--- src/components/prevWork.vue
+++ src/components/prevWork.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="tile is-ancestor" id="usedDiv">
+    <div class="tile is-ancestor" id="workDiv">
       <div class="tile is-parent is-4" v-for="site in sites" v-bind:key="site.id">
         <a v-bind:href="site.link" target="_blank">
           <article class="tile is-child box workBox" :style="{background: 'url('+site.image+')'}">
