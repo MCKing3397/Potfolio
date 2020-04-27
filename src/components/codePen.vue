93dc9d1c6535fc1d8abdfe84d502afbf9afd0342
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-26 20:33:40 -0500

    Update Codepens layout

--- src/components/codePen.vue
+++ src/components/codePen.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="tile is-ancestor" id="codePens">
       <div class="tile is-parent is-6" v-for="pen in pens" v-bind:key="pen.id">
-        <p class="codepen" data-theme-id="default" data-default-tab="result" data-user="Michael_King" v-bind:data-slug-hash="pen.hash" style="height: 265px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;" v-bind:data-pen-title="pen.title">
+        <p class="codepen" data-theme-id="default" data-default-tab="result" data-user="Michael_King" v-bind:data-slug-hash="pen.hash" style="height: 100%; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;" v-bind:data-pen-title="pen.title">
         <span>See the Pen <a v-bind:href="pen.url">
         {{ pen.Title }}</a> by Michael (<a href="https://codepen.io/Michael_King">@Michael_King</a>)
         on <a href="https://codepen.io">CodePen</a>.</span>
