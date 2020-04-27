0f0679a51064a3a15fbb071fb989c2786bd6f418
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-04-26 23:06:00 -0500

    Remove Carousel and see how tiles work

--- src/components/prevWork.vue
+++ src/components/prevWork.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="container">
-    <carousel :scrollPerPage="true" :perPageCustom="[[480, 2], [768, 3]]"> 
-      <slide><img src="https://picsum.photos/300/100/"></slide>
-      <slide><img src="https://picsum.photos/300/100/"></slide>
-      <slide><img src="https://picsum.photos/300/100/"></slide>
-      <slide><img src="https://picsum.photos/300/100/"></slide>
-      <slide><img src="https://picsum.photos/300/100/"></slide>
-    </carousel>
+    <div class="tile is-ancestor" id="usedDiv">
+      <div class="tile is-parent is-4" v-for="site in sites" v-bind:key="site.id">
+        <a v-bind:href="site.link" target="_blank">
+          <article class="tile is-child box">
+            <figure class="image">
+              <img v-bind:src="site.image">
+            </figure>
+            <p class="content has-text-centered"><span class="title">{{ site.title }}</span></p>
+          </article>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 <script>
