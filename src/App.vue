cea4b97e2f427e21df85034734c7a59711bd7e5a
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-08-07 16:52:07 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -175,6 +175,8 @@
     ]"
     />
 
+    <Contact />
+
   </div>
 </template>
 
@@ -184,7 +186,7 @@ import Navbar from './components/page_top.vue'
 import UsedTech from './components/Used.vue'
 import codePen from './components/codePen.vue'
 import prevWork from './components/prevWork.vue'
-//import Contact from './components/contact.vue'
+import Contact from './components/contact.vue'
 //import Footer from './components/Footer.vue'
 
 export default {
@@ -194,8 +196,8 @@ export default {
     DataBox,
     UsedTech,
     prevWork,
-    codePen
-    //Contact
+    codePen,
+    Contact
     //Footer
   }
 }
