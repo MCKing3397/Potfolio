476d7774c0bb9fef9106f03c2089e9ab3631643d
MCKing3397 <56730771+MCKing3397@users.noreply.github.com>
2020-08-14 15:45:12 -0500

    Add vue gtag

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import Header from './Head.vue'
 import Footer from './Footer.vue'
 import vueSmoothScroll from 'vue2-smooth-scroll'
+import VueGtag from "vue-gtag"
 import 'bulma/css/bulma.css'
 Vue.use(vueSmoothScroll)
+
+Vue.use(VueGtag, {
+  config: { id: "UA-175459186-1" }
+});
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
